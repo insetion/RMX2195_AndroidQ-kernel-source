fb3cb908fb8dc56294318bafdd74ac04c0b0ab41
insetion <69282747+insetion@users.noreply.github.com>
2025-02-05 01:40:14 +0800

    Update fpsensor_spi_tee.c

--- drivers/input/oppo_fp_drivers/chipone/fpsensor_spi_tee.c
+++ drivers/input/oppo_fp_drivers/chipone/fpsensor_spi_tee.c
@@ -739,9 +739,9 @@ static int fpsensor_remove(struct platform_device *pdev)
     if (fpsensor_dev->irq)
         free_irq(fpsensor_dev->irq, fpsensor_dev);
 
-#if FP_NOTIFY
+/*#if FP_NOTIFY
     fpsensor_fb_unregister_client(&fpsensor_dev->notifier);
-#endif
+#endif*/
     fpsensor_gpio_free(fpsensor_dev);
     fpsensor_dev_cleanup(fpsensor_dev);
 #if FPSENSOR_WAKEUP_TYPE == FPSENSOR_WAKEUP_SOURCE
